0e0bd9d0ccf3ad0a0480e2bb12eaeac0431a9d48
Ronnie <ronaldabel1996@gmail.com>
2024-08-22 05:09:05 +0000

    SaaS Flash codebase v1.5

--- app/api/checkout_session/route.js
+++ app/api/checkout_session/route.js
@@ -1,6 +1,8 @@
 import { NextResponse } from "next/server";
 import Stripe from "stripe";
 
+const Stripe = require('stripe');
+
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, { apiVersion: '2022-11-15' });
 
 const formatAmountForStripe = (amount, currency) => {
